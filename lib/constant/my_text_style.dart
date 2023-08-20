97b56294b5946587efa44328bd59d778af9c2025
ko_cha78 <kota1725@gmail.com>
2023-08-20 23:53:09 +0900

    create item detail page

---
+++ lib/constant/my_text_style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:trade_app/constant/my_colors.dart';
+
+class MyTextStyles {
+  static const mediumBold = TextStyle(
+    color: MyColors.primary,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static const mediumNormal = TextStyle(
+    color: MyColors.primary,
+    fontSize: 16,
+  );
+}
