1154fd85c6877daa8a96e0d9d2a7f99c238051b2
ko_cha78 <kota1725@gmail.com>
2024-01-04 01:51:10 +0900

    custom_text_form_fieldの追加

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:trade_app/component/custom_text_form_field.dart';
 import 'package:trade_app/config/user_data_provider.dart';
 import 'package:trade_app/config/user_preferences.dart';
 import 'package:trade_app/constant/my_colors.dart';
+import 'package:trade_app/constant/my_text_style.dart';
 import 'package:trade_app/constant/texts.dart';
 import 'package:trade_app/views/navigation_root/navigation_root.dart';
 
@@ -17,6 +19,8 @@ class _LoginViewState extends ConsumerState<LoginView> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   bool _isObscure = true;
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,82 +33,110 @@ class _LoginViewState extends ConsumerState<LoginView> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: AutofillGroup(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const Spacer(),
-                TextField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: Texts.emailHintText,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Text(
+                      Texts.emailLabelText,
+                      style: MyTextStyles.mediumNormal.copyWith(
+                        color: MyColors.black,
+                      ),
+                    ),
+                  ),
+                  CustomTextFormField(
+                    hasObscureIcon: false,
+                    controller: _emailController,
                     hintText: Texts.emailHintText,
-                  ),
-                  autofillHints: const [AutofillHints.email],
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                const SizedBox(height: 16.0),
-                TextField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: Texts.passwordHintText,
-                    hintText: Texts.passwordHintText,
-                    suffixIcon: IconButton(
-                      icon: Icon(_isObscure ? Icons.visibility_off : Icons.visibility),
-                      onPressed: () {
-                        setState(() {
-                          _isObscure = !_isObscure;
-                        });
-                      },
-                    ),
-                  ),
-                  autofillHints: const [AutofillHints.password],
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: _isObscure,
-                ),
-                const Spacer(),
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: MyColors.tertiary,
-                      foregroundColor: MyColors.white,
-                    ),
-                    onPressed: () async {
-                      ref.watch(userDataProvider.notifier).setMailAndPW(
-                        _emailController.text,
-                        _passwordController.text,
-                      );
-                      final isLogin = await ref.watch(userDataProvider.notifier).login(
-                        context: context,
-                        errMsg: 'エラー 再度ログインしてください。',
-                      );
-                      final userDataState = ref.read(userDataProvider);
-                      if (isLogin) {
-                        // ユーザーデータをShared Preferencesに保存
-                        UserPreferences.saveUserData(
-                          id: userDataState.id,
-                          email: userDataState.email,
-                          password: userDataState.password,
-                        );
-                        Future(() {
-                          Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const NavigationRoot(),
-                            ),
-                                (_) => false,
-                          );
-                        });
+                    isObscure: false,
+                    onToggleObscure: (value) {},
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
                       }
+                      return null;
                     },
-                    child: const Text(Texts.login),
+                    autofillHints: const [AutofillHints.email],
+                    keyboardType: TextInputType.emailAddress,
                   ),
-                ),
-                const SizedBox(height: 16),
-                const Spacer(),
-              ],
+                  const SizedBox(height: 4),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Text(
+                      Texts.passwordLabelText,
+                      style: MyTextStyles.mediumNormal.copyWith(
+                        color: MyColors.black,
+                      ),
+                    ),
+                  ),
+                  CustomTextFormField(
+                    hasObscureIcon: true,
+                    controller: _passwordController,
+                    hintText: Texts.passwordHintText,
+                    isObscure: _isObscure,
+                    onToggleObscure: (value) {
+                      setState(() {
+                        _isObscure = value;
+                      });
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    autofillHints: const [AutofillHints.password],
+                    keyboardType: TextInputType.visiblePassword,
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: MyColors.tertiary,
+                        foregroundColor: MyColors.white,
+                      ),
+                      onPressed: () async {
+                        _formKey.currentState!.validate();
+                        ref.watch(userDataProvider.notifier).setMailAndPW(
+                              _emailController.text,
+                              _passwordController.text,
+                            );
+                        final isLogin =
+                            await ref.watch(userDataProvider.notifier).login(
+                                  context: context,
+                                  errMsg: 'エラー 再度ログインしてください。',
+                                );
+                        final userDataState = ref.read(userDataProvider);
+                        if (isLogin) {
+                          // ユーザーデータをShared Preferencesに保存
+                          UserPreferences.saveUserData(
+                            id: userDataState.id,
+                            email: userDataState.email,
+                            password: userDataState.password,
+                          );
+                          Future(() {
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const NavigationRoot(),
+                              ),
+                              (_) => false,
+                            );
+                          });
+                        }
+                      },
+                      child: const Text(Texts.login),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const Spacer(),
+                ],
+              ),
             ),
           ),
         ),
